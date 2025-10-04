a821d6dee2587f182a8a346fc23d4d20e5f1358c
khairayubi <khairullah3412@gmail.com>
2025-10-05 01:43:49 +0500

    80% weather app completed

--- app.js
+++ app.js
@@ -1,14 +1,37 @@
+const div = document.querySelector(".container");
 // fetch('http://api.weatherapi.com/v1/current.json?key=b4db485d7c4c485fa6d84351232508&q=${city.value}&aqi=no')
-fetch('http://api.weatherapi.com/v1/current.json?key=9b05f55551e14d4fbaa151508250701&q=canada&aqi=no')
+const form = document.getElementById("weatherForm");
+const names = [];
+form.addEventListener("submit", (e) => {
+    e.preventDefault(); // stop form refresh
+    const city = document.getElementById("city").value;
 
-.then(res =>{
-    return res.json()
-})
-.then(res=>{
-    console.log(res);
-    
-})
-.catch(err =>{
-    console.log(err);
-    
-})
+
+
+    fetch(`http://api.weatherapi.com/v1/current.json?key=9b05f55551e14d4fbaa151508250701&q=${city}&aqi=no`)
+
+        .then(res => {
+            return res.json()
+        })
+        .then(res => {
+            console.log(res);
+
+            div.innerHTML += `
+            <div class="card">
+                    <div class="details">
+                        <h1>${res.location.name}</h1>
+                        <h3>${res.location.region}, <span>${res.location.country}</span></h3>
+                        <h2>${res.current.temp_c}</h2>
+                        <h4>${res.location.localtime}</h4>
+                    </div>
+                    <div class="image"><img src="https:${res.current.condition.icon}" alt="">
+                        <p>${res.current.condition.text}</p>
+                    </div>
+        </div>`
+        })
+
+        .catch(err => {
+            console.log(err);
+
+        })
+});
